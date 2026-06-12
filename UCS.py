bd2143afc8ee5219924e34add94f07abefe7d9e6
Jaideepsinh Raulji <92981558+jaideepsinhraulji@users.noreply.github.com>
2026-06-13 00:30:16 +0530

    Update UCS.py

--- UCS.py
+++ UCS.py
@@ -1,45 +1,32 @@
-#UCS with incurring cumulative weights
-from queue import PriorityQueue
-graph = {
-    'A': {'B': 2, 'C': 1},
-    'B': {'D': 3, 'E': 4},
-    'C': {'F': 5},
-    'D': {'G': 6},
-    'E': {'G': 7},
-    'F': {'G': 8},
-    'G': {}
-}
-# graph = {
-#     'A': {'B': 2, 'C': 1},
-#     'B': {'D': 4},
-#     'C': {'D': 3},
-#     'D': {}
-# }
-
-visitedpq = PriorityQueue() # List for visited nodes.
-
-def ucs(graph, node, goal): #function for BFS
-  pq=PriorityQueue()  #Initialize a queue  
-  pq.put([0,node])
-  visitedpq.put([0,node])
-  tn='x'
-  tw=0
-  while not pq.empty():#empty returns true if empty     
-    tw,tn=pq.get()
-    visitedpq.put([tw,tn])
-    if tn==goal:
-        break
-    else:
-        for neigh,weight in graph[tn].items():  
-            pq.put([weight+tw,neigh])
-        #visited.append(tn)
+#updated UCS (cumulative cost)
+def ucs(graph, start, goal):
+    pq = [[0,start]]
+    visited = []
+    while pq:
+        g,cnode = pq.pop(0)
+        visited.append([g,cnode])
+        #print("visited ",visited)
+        if cnode[-1]==goal:
+            return visited[-1]
         
-# Driver Code
-print("Following is the UCS")
-ucs(graph, 'A','D')    # A is start state and G is a goal node
-while not visitedpq.empty():
-    tw,tn = visitedpq.get()
-    if(tn=='D'):
-        print(tn)
-        break
-    print(tn)
+        for neigh, wt in graph[cnode[-1]].items(): 
+            g1=g+wt
+            path=cnode+neigh
+            pq.append([g1,path])
+            pq=sorted(pq)
+    
+graph = {
+     'A': {'B': 3, 'C': 2},
+     'B': {'D': 4, 'E': 5},
+     'C': {'F': 10},
+     'D': {'G': 3},
+     'E': {'G': 1},
+     'F': {'G': 1},
+     'G': {}
+}
+start = 'A'
+goal = 'G'
+
+visitedf=(ucs(graph, start, goal))
+print("The shortest Path is ",visitedf)
+
